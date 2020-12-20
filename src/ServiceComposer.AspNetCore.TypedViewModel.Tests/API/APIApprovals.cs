a64be62e4fdc3a9bc7a3b8089905b0a87f85d55b
Mauro Servienti <mauro.servienti@gmail.com>
2020-12-20 09:19:05 +0100

    Approved API

---
+++ src/ServiceComposer.AspNetCore.TypedViewModel.Tests/API/APIApprovals.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+using ApprovalTests;
+using ApprovalTests.Namers;
+using ApprovalTests.Reporters;
+using PublicApiGenerator;
+using Xunit;
+
+namespace ServiceComposer.AspNetCore.TypedViewModel.Tests.API
+{
+    public class APIApprovals
+    {
+        [Fact]
+        [UseReporter(typeof(DiffReporter))]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        #if NETCOREAPP3_1
+        [UseApprovalSubdirectory("NETCOREAPP3_1")]
+        #endif
+        #if NET5_0
+        [UseApprovalSubdirectory("NET5_0")]
+        #endif
+        public void Approve_API()
+        {
+            var publicApi = typeof(CastleDynamicProxyViewModelFactory).Assembly.GeneratePublicApi();
+
+            Approvals.Verify(publicApi);
+        }
+    }
+}
